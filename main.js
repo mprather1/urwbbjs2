177bb5196a7cdfc8e6040e712205b380de76b638
Michael Prather <mprather1@gmail.com>
2016-04-25 19:06:26 -0500

    looking for changes in user model

--- main.js
+++ main.js
@@ -2,10 +2,25 @@
 singleUser = Backbone.Model.extend({
   defaults: {
     name: "killb"
+  },
+
+  initialize: function() {
+
+    console.log("A model instance named " + this.get("name") + " has been created");
+
+    this.on('change', function() {
+      console.log("Something in our model has changed.");
+    });
+
+    this.on('change:name', function() {
+      console.log("The name for the " + this.get("name") + " model has changed");
+    });
   }
 });
 
 // instantiate
 var mike = new singleUser({
-  name: "Michael Prather"
+  name: "Mike Prather"
 });
+
+mike.set('name', "Michael Prather")
